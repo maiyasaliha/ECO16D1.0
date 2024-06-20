9cfed9565fba682a7d3358e712807446a5e2e9e5
unknown <s.maiya095@gmail.com>
2024-06-20 10:37:36 +0800

    added conditional formatting for checkbox and select

--- client/eco16d/src/components/Spreadsheet.js
+++ client/eco16d/src/components/Spreadsheet.js
@@ -4,21 +4,10 @@ import 'handsontable/dist/handsontable.full.css';
 import axios from 'axios';
 import io from 'socket.io-client';
 import './styles.css';
+import { nestedHeaders, columns, getColorClassForCb, getColorClassForDd } from './PrincipaleSheetStructure';
 
 const socket = io('http://localhost:3001');
 
-function getColorClassForValue(value) {
-    switch (value) {
-        case 'Yes':
-            return 'custom-cell-yes';
-        case 'No':
-            return 'custom-cell-no';
-        case 'Standby':
-            return 'custom-cell-standby';
-        default:
-            return '';
-    }
-}
 
 function Spreadsheet() {
     const [hotInstance, setHotInstance] = useState(null);
@@ -57,44 +46,6 @@ function Spreadsheet() {
     useEffect(() => {
         if (hotElementRef.current && data.length > 0 && !hotInstance) {
 
-            const nestedHeaders = [
-                [{ label: 'le rouge suggère un identifiant de commande incorrect de plus de 8 caractères', colspan: 16, className: 'header-group-style5' }, 
-                    { label: 'le vert suggère que le numéro de commande existe déjà dans la page colis manquants', colspan: 6, className: 'header-group-style4' }],
-                [{ label: 'Last Updated [GMT+8]', colspan: 2, className: 'header-group-style1' }, { label: '16/05/2024 16:03:39', colspan: 2, className: 'header-group-style5' },
-                     { label: 'ECO À REMPLIR', colspan: 12, className: 'header-group-style3' }, { label: 'AXE À REMPLIR', colspan: 6, className: 'header-group-style4' }],
-                [
-                    'date ajoutée', 'BMID', 'Nom du client', 'Raison du retour', 'BM Raison du retour', 'SKU', 'Nom du produit', 
-                    'IMEI', 'Transporteur', 'Numéro de suivi', 'Customer Informed about non-compliance', 'Customer informed if locked?', 
-                    'Refunded', 'Returned to SG', 'Date returned to SG', 'Waybill No.', 'Date de réception Axe', 'Contenu conforme ?', 
-                    'IMEI de réception', 'État de l`appareil', 'Commentaires (Rayures ? Bosses ?)', 
-                    'Lien Google pour les images (https://drive.google.com/drive/folders/1SNzn0LkNwHqi_IO4i-FcuWr7ytwRjS3D?usp=sharing)'
-                ],
-            ]
-            const columns = [
-                { type: 'date', dateFormat: 'DD/MM/YYYY' }, // date ajoutée
-                { type: 'numeric' }, // BMID
-                { type: 'text' }, // Nom du client
-                { type: 'text' }, // Raison du retour
-                { type: 'text' }, // BM Raison du retour
-                { type: 'text' }, // SKU
-                { type: 'text' }, // Nom du produit
-                { type: 'text' }, // IMEI
-                { type: 'text' }, // Transporteur
-                { type: 'text' }, // Numéro de suivi
-                { type: 'checkbox', className: 'htCenter htMiddle' }, // Customer Informed about non-compliance
-                { type: 'checkbox', className: 'htCenter htMiddle' }, // Customer informed if locked?
-                { type: 'checkbox', className: 'htCenter htMiddle' }, // Refunded
-                { type: 'checkbox', className: 'htCenter htMiddle' }, // Returned to SG
-                { type: 'date', dateFormat: 'MM/DD/YYYY' }, // Date returned to SG
-                { type: 'numeric' }, // Waybill No.
-                { type: 'date', dateFormat: 'DD/MM/YYYY' }, // Date de réception Axe
-                { type: 'dropdown', source: ['', 'Yes', 'No', 'Standby'] }, // Contenu conforme?
-                { type: 'text' }, // IMEI de réception
-                { type: 'dropdown', source: ['', 'Unlocked', 'LOCKED', 'Standby'] }, // État de l`appareil
-                { type: 'text' }, // Commentaires (Rayures ? Bosses ?)
-                { type: 'text' }, // Lien Google pour les images
-            ];
-
             const mappedData = data.map(row => [
                 row.dateAjoutee,
                 row.BMID,
@@ -127,17 +78,19 @@ function Spreadsheet() {
                 nestedHeaders: nestedHeaders,
                 customBorders: customBorders,
                 columns: columns,
-                cells: (row, col, prop) => {
-                    const cellProperties = {};
+                afterGetCellMeta: function (row, col, cellProperties) {
+                    const cellValue = this.getDataAtCell(row, col);
+                    let cellClass;
 
-                    if (col === 11) {
-                        const cellValue = hot.getDataAtCell(row, col);
-                        const cellClass = getColorClassForValue(cellValue);
-                        cellProperties.className = cellClass;
-                        //cellProperties.className = 'custom-cell-yes';
+                    if (col === 17 || col === 19) {
+                        cellClass = getColorClassForDd(cellValue);
                     }
+                    if (col === 10 || col === 11 || col === 12 || col === 13) {
+                        cellClass = getColorClassForCb(cellValue);
+                        console.log(cellValue);
+                    }
+                    cellProperties.className = cellClass;
 
-                    return cellProperties;
                 },
                 contextMenu: true,
                 dropdownMenu: true,
