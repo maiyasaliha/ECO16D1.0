a216b7e1adf2320b1f157f59b49f19a2d537e85c
unknown <s.maiya095@gmail.com>
2024-06-21 12:05:42 +0800

    move link renderer to separate file

--- client/eco16d/src/components/Principale/PrincipaleSheetStructure.js
+++ client/eco16d/src/components/Principale/PrincipaleSheetStructure.js
@@ -1,5 +1,4 @@
-import Handsontable from 'handsontable';
-
+import { linkRenderer } from "./LinkRenderer";
 const nestedHeaders = [
     [
         { label: 'le rouge suggère un identifiant de commande incorrect de plus de 8 caractères', colspan: 16 }, 
@@ -43,17 +42,5 @@ const columns = [
     { type: 'text' }, // Commentaires (Rayures ? Bosses ?)
     { type: 'text', renderer: linkRenderer }, // Lien Google pour les images
 ];
-
-function linkRenderer(instance, td, row, col, prop, value, cellProperties) {
-    Handsontable.renderers.TextRenderer.apply(this, arguments);
-
-    if (value && typeof value === 'string' && value.startsWith('http')) {
-        td.innerHTML = `<a href="${value}" target="_blank">${value}</a>`;
-    } else {
-        td.innerHTML = value;
-    }
-
-    return td;
-}
 
 export {nestedHeaders, columns};
