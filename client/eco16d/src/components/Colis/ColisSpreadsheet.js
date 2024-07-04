19044ea04e3e3001904ef1032d9d02551f127b33
unknown <s.maiya095@gmail.com>
2024-07-04 12:35:53 +0800

    add real time collaboration for colis

--- client/eco16d/src/components/Colis/ColisSpreadsheet.js
+++ client/eco16d/src/components/Colis/ColisSpreadsheet.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState, useRef } from 'react';
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.css';
 import axios from 'axios';
+import io from 'socket.io-client';
 import { useSearchParams } from 'react-router-dom';
 import './colisStyles.css';
 import { nestedHeaders, getColumns } from './ColisSheetStructure';
 import { getColorClassForBMID } from './ConditionalColoring'
 import ToolBar from '../ToolBar';
 import { useDate } from '../../contexts/DateContext';
+import { useAuth } from '../../contexts/AuthContext';
+
+const socket = io('http://localhost:3001');
 
 function ColisSpreadsheet() {
     const [hotInstance, setHotInstance] = useState(null);
@@ -22,9 +26,41 @@ function ColisSpreadsheet() {
 
     const organisation = searchParams.get('organisation');
     const { year, quarter } = useDate();
-    console.log(year);
-    console.log(quarter);
-    
+    const { userData } = useAuth();
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log( userData?.name + ' Connected to Socket.IO server');
+        });
+
+        socket.on('cellUpdate', (data) => {
+
+            if (hotInstance) {
+                const { rowIndex, colIndex, newValue } = data.updateData;
+
+                if (newValue !== data.previousData.value) {
+                    console.log("setting");
+                    hotInstance.setDataAtCell(rowIndex, colIndex, newValue);
+                }
+            }
+        });
+
+        socket.on('connect_error', (error) => {
+            console.error('Socket.IO connection error:', error);
+        });
+
+        socket.on('disconnect', () => {
+            console.log(userData?.name + ' Disconnected from Socket.IO server');
+        });
+
+        return () => {
+            socket.off('connect');
+            socket.off('cellUpdate');
+            socket.off('connect_error');
+            socket.off('disconnect');
+        };
+    }, [hotInstance, userData, data]);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -99,14 +135,19 @@ function ColisSpreadsheet() {
                 colWidths: 120,
                 allowHtml: true,
                 afterChange: (changes, source) => {
+                    let updateData;
+                    let previousData;
                     if (source !== 'loadData' && changes) {
                         const updateRequests = changes.map(change => {
-                            const updateData = {
+                            updateData = {
                                 rowIndex: change[0],
                                 colIndex: change[1],
                                 newValue: change[3] == null ? "" : change[3]
                             };
-                
+                            previousData = {
+                                value: change[2]
+                            }
+                            socket.emit('cellUpdate', {updateData, previousData});
                             return axios.post('http://localhost:3001/colisCell', updateData);
                         });
                 
