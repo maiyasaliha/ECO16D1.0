a312eb7c028949a0b2567afcf5e35d6884c7a549
unknown <s.maiya095@gmail.com>
2024-07-29 11:04:09 +0800

    saving,, im moving back to the prv one

--- client/eco16d/src/components/ToolBar.jsx
+++ client/eco16d/src/components/ToolBar.jsx
@@ -12,24 +12,20 @@ function ToolBar({ principale, eco, colis, selectedCell }) {
   const organisation = userData?.organisation;
 
   const { year, setYear, quarter, setQuarter, newPage, setnewPage } = useDate();
-  const [quarterClick, setquarterClick] = useState(true);
 
   const onQuarterClick = (selectedQuarter) => () => {
     setQuarter(selectedQuarter);
-    setquarterClick(true);
     setnewPage(false);
     console.log("clicking quarter: " + selectedQuarter)
   };
 
   const onYearClick = (selectedYear) => () => {
     setYear(selectedYear);
-    newPage ? setquarterClick(false) : setquarterClick(true);
     console.log("clicking year: " + selectedYear)
   };
 
-  const onNewClick = () => {
-    setnewPage(!newPage);
-    setquarterClick(false);
+  const onNewClick = (pressed) => () => {
+    setnewPage(pressed);
     console.log("clicking new: ")
   }
 
@@ -45,52 +41,64 @@ function ToolBar({ principale, eco, colis, selectedCell }) {
   return (
     <div className='toolbar'>
       <div>
-        <Button>
-          <Link to="/home">HOME</Link>
-        </Button>
-        <Button type={principale ? 'primary' : 'default'}>
-          <Link to={`/principale?organisation=${organisation}`}>PRINCIPALE</Link>
-        </Button>
-        {
-          organisation === 'ECO' &&
-          <Button type={eco ? 'primary' : 'default'}>
-            <Link to="/eco">ECO</Link>
+        <div>
+          <Button>
+            <Link to="/home">HOME</Link>
           </Button>
-        }
-        <Button type={colis ? 'primary' : 'default'}>
-          <Link to={`/colis?organisation=${organisation}`}>COLIS MANQUANTS</Link>
-        </Button>
+          <Button onClick={onNewClick(false)} type={!newPage && principale ? 'primary' : 'default'}>
+            <Link to={`/principale?organisation=${organisation}`}>PRINCIPALE</Link>
+          </Button>
+          {
+            organisation === 'ECO' &&
+            <Button onClick={onNewClick(false)} type={!newPage && eco ? 'primary' : 'default'}>
+              <Link to="/eco">ECO</Link>
+            </Button>
+          }
+          <Button onClick={onNewClick(false)} type={!newPage && colis ? 'primary' : 'default'}>
+            <Link to={`/colis?organisation=${organisation}`}>COLIS MANQUANTS</Link>
+          </Button>
+          {!eco ? 
+          <div>
+            <Button
+              type={newPage ? 'primary' : 'default'}
+              icon={<PlusOutlined />}
+              onClick={onNewClick(true)}
+            >
+              New
+            </Button> 
+            { newPage ?
+              <Button onClick={onNewClick(false)} type={!newPage && colis ? 'primary' : 'default'}>
+                {principale ? <Link to="/newPrincipale">New Colis</Link> : colis? <Link to="/newColis">New Pri</Link> : null}
+              </Button>
+              : null
+            }
+          </div>
+          : null}
+        </div>
       </div>
       {eco || newPage ? null : <VersionHistoryOverlay selectedCell={selectedCell} sheet={findSheet()} />}
       <div>
-      {!eco ? <Button
-          type={newPage ? 'primary' : 'default'}
-          icon={<PlusOutlined />}
-          onClick={onNewClick}
-        >
-          {principale ? <Link to="/newPrincipale">New</Link> : colis? <Link to="/newColis">New</Link> : null}
-        </Button> : null}
         <Button onClick={onYearClick(year - 1)}>{year - 1}</Button>
         <Button
-          type={quarterClick && quarter === 1 ? 'primary' : 'default'}
+          type={!newPage && quarter === 1 ? 'primary' : 'default'}
           onClick={onQuarterClick(1)}
         >
           Jan-Mar
         </Button>
         <Button
-          type={quarterClick && quarter === 2 ? 'primary' : 'default'}
+          type={!newPage && quarter === 2 ? 'primary' : 'default'}
           onClick={onQuarterClick(2)}
         >
           Apr-Jun
         </Button>
         <Button
-          type={quarterClick && quarter === 3 ? 'primary' : 'default'}
+          type={!newPage && quarter === 3 ? 'primary' : 'default'}
           onClick={onQuarterClick(3)}
         >
           Jul-Sep
         </Button>
         <Button
-          type={quarterClick && quarter === 4 ? 'primary' : 'default'}
+          type={!newPage && quarter === 4 ? 'primary' : 'default'}
           onClick={onQuarterClick(4)}
         >
           Oct-Dec
